f2c679a8b30c674f3c852047af03dce53bdec29b
kalmuroth <87120289+kalmuroth@users.noreply.github.com>
2021-10-14 09:50:17 +0200

    day 1

---
+++ day-1/exercice-5.js
@@ -0,0 +1,11 @@
+export function my_array_alpha(str) {
+    var len = 0;
+    let array = [];
+    while (str[len] !== undefined){
+      array[len] = str[len];
+      len++;
+    }
+    return array;
+  }
+
+
