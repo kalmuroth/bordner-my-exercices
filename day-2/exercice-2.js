0e15712e8be72ce1333a666351204896bea9485f
kalmuroth <lucas.bordner@cloudartifact.com>
2021-10-14 11:11:37 +0200

    exo 2 V0.4

---
+++ day-2/exercice-2.js
@@ -0,0 +1,10 @@
+export function my_display_multi_42(multi, int){
+    var multiplication = multi * int;
+    if (multiplication == 42){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
