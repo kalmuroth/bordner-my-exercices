72a72e4694459c36ba5f4d3c427137b456f6ffd9
kalmuroth <lucas.bordner@cloudartifact.com>
2021-10-14 11:59:57 +0200

    exo 2 V0.6

--- day-1/exercice-1.js
+++ day-1/exercice-1.js
@@ -1,1 +1,2 @@
 export const my_display_alpha = () => ("abcdefghijklmnopqrstuvwxyz");
+
--- day-1/exercice-7.js
+++ day-1/exercice-7.js
@@ -1,8 +1,8 @@
 export function my_is_posi_neg(nbr){
-    if (nbr > 0 && nbr === 0 && nbr !== undefined)
+    if (nbr > 0)
         return "POSITIVE ";
     if (nbr < 0)
         return "NEGATIVE";
-    if (nbr === 0 && nbr === undefined && nbr < 0)
+    if (nbr === 0)
         return "NEUTRAL";
 }
